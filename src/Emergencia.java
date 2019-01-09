8ce9ed0899f1a3c6f3da19940e74c7c26e7d9dd9
joao <joao182cx@gmail.com>
2019-01-09 15:00:28 -0200

    Criação da classe Emergencia, com os seus respectivos atributos.

---
+++ src/Emergencia.java
@@ -0,0 +1,18 @@
+import java.sql.Time;
+
+public class Emergencia {
+
+	/*
+	 local
+	 data/hora
+	 natureza da emergencia
+	 tipo e status da resposta
+	 */
+	
+	private String local;
+	private Time dataEHora;
+	private NaturezaDaEmergencia natureza;
+	private Tipo tipo;
+	private Status status;
+	
+}
